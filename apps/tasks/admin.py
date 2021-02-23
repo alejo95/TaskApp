6c5f2d158939c292d7e08e2685248229163abcbf
alejandro ramirez cobos <alejo@alejandros-MacBook-Pro.local>
2021-02-23 16:19:44 -0500

    Creacion de vista generica post tasks

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from apps.tasks.models import *
 
+class CategoryTaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description')
 
-admin.site.register(CategoryTask)
+admin.site.register(CategoryTask, CategoryTaskAdmin)
 admin.site.register(Tasks)
--- apps/tasks/api/views/task_views.py
+++ apps/tasks/api/views/task_views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from apps.base.api import GeneralListApiView
 from apps.tasks.api.serializers.task_serializers import TaskSerializer
@@ -10,5 +11,32 @@ class TaskListAPIView(GeneralListApiView):
 class TaskCreateAPIView(generics.CreateAPIView):
     serializer_class = TaskSerializer
 
+    def post(self, request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Tarea creada correctamente'}, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
 
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
